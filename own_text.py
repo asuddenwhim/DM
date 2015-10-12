fe7dbd704a1b1de831a11ad27696447ab07e03c3
asuddenwhim <man810asa@gmail.com>
2015-10-12 12:29:34 +0200

    hi 5

--- own_text.py
+++ own_text.py
@@ -1,3 +1,4 @@
+
 from nltk import word_tokenize, sent_tokenize
 text = "Generally, data mining (sometimes called data or knowledge discovery) is the process of analyzing data from different perspectives and summarizing it into useful information - information that can be used to increase revenue, cuts costs, or both. Data mining software is one of a number of analytical tools for analyzing data. It allows users to analyze data from many different dimensions or angles, categorize it, and summarize the relationships identified. Technically, data mining is the process of finding correlations or patterns among dozens of fields in large relational databases. "
 sents = sent_tokenize(text)
--- stopwords.py
+++ stopwords.py
@@ -7,9 +7,16 @@ stop_words = set(stopwords.words("english"))
 words = word_tokenize(text)
 
 filtered_sentence = []
+filtered_again = []
 
 for w in words:
 	if w not in stop_words:
 		filtered_sentence.append((w))
 
 print filtered_sentence
+
+for w in words:
+	if not w in stop_words:
+		filtered_again.append(w)
+
+print filtered_again
